b26b6eebda22372a57d60d70091ae5fa44c63b9f
eneax <19818937+eneax@users.noreply.github.com>
2017-12-15 21:19:48 +0100

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+  // Sticky Nav
+  $('.section-home-js').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px;'
+  });
+
+  // Scroll Nav
+  // It selects the "a" element that have an href that starts with "#"
+  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  // Mobile Nav
+  $('.nav-icon-js').click(function() {
+    var nav = $('.main-nav-js');
+    var icon = $('.nav-icon-js i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  });
+  
+});
